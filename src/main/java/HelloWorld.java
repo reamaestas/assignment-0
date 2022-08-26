6a397af0b68140d51bbb19b5ca3f9e10b4669c80
reamaestas <reamaestas@yahoo.com>
2022-08-26 15:26:34 -0500

    completed assignment 0

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -6,9 +6,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println(sayHello());
     }
-
     public static String sayHello() {
-        return "";
+        return "Hello, World!";
     }
 
 }
